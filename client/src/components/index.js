54a1d16ed941b0829606dc8d065c56d99092bd66
MeryAmun <chebesiawah@gmail.com>
2023-05-02 12:52:13 +0100

    welcome section done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,8 @@ export {default as Welcome } from './Welcome'
 export {default as Loader } from './Loader'
 export {default as Services } from './Services'
 export {default as Transactions } from './Transactions'
+
+
+export const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+
