f8a73a4217337d185f9c745d7e2ceb368f2f91a5
MeryAmun <chebesiawah@gmail.com>
2023-05-04 14:33:59 +0100

    deployed smart contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x22dcBAAC21477F620Eb7184CAdbcFA8495c76e72'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-sepolia.g.alchemy.com/v2/lDQxQsG7rSxXfhdZpP9P1CeMB2d5UfJT
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle');
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks:{
+    sepolia:{
+      url:'https://eth-sepolia.g.alchemy.com/v2/lDQxQsG7rSxXfhdZpP9P1CeMB2d5UfJT',
+      accounts:['431be59e00dce596f60fa962c54c381fa0fb2d38af69ed12225d02e05a2d4e41']
+    }
+  }
+}
