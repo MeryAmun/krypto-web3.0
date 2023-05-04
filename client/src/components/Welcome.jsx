454990d2fe3de96c58ba4aacbfee33baa70d9de6
MeryAmun <chebesiawah@gmail.com>
2023-05-04 19:53:01 +0100

    send transactions working

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -10,7 +10,12 @@ const  Welcome = () => {
   const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
 const [loading, setLoading] = useState(false)
 
-  const handleSubmit = () => {}
+  const handleSubmit = (e) => {
+    const {addressTo, amount, keyword, message} = formData;
+    e.preventDefault();
+    if(!addressTo || !amount || !keyword || !message) return;
+    sendTransaction();
+  }
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -21,9 +26,13 @@ const [loading, setLoading] = useState(false)
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
           </p>
-          <button type="button"  className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" onClick={connectWallet}>
+          {
+            !currentAccount && (
+              <button type="button"  className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" onClick={connectWallet}>
             <p className="text-white text-base font-semibold">Connect Wallet</p>
           </button>
+            )
+          }
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
                Reliability
